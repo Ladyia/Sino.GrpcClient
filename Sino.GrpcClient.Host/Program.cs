6f499fa3ba50b189afcc593ae390104186e2357c
姚正发 <976691141@qq.com>
2016-10-21 19:49:59 +0800

    init

---
+++ Sino.GrpcClient.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sino.GrpcClient.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    var list = MsgServiceClient.GetList(0, "1", 0, 0);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
